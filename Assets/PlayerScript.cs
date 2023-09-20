522eb53d134471fe686c01eb01fdb38285f4b6c8
Hayden Ross <haydenross00@gmail.com>
2023-09-20 17:50:27 -0400

    Finished and ready to be built for web

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+
+
+using System.Runtime.InteropServices;
+
 public class SnakeMovement : MonoBehaviour
 {
     public Transform movePoint;
